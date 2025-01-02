9fd9e9ea0f56505abf7bd1d71bd1a7dc6fddbc8f
zoupeicheng <zoupeicheng.zpc@antgroup.com>
2025-01-02 15:21:16 +0800

    fss first version

--- paper_code/fss.py
+++ paper_code/fss.py
@@ -10,6 +10,7 @@ jax.config.update("jax_enable_x64", True)
 
 GROUP_BIT_NUM = 64
 
+
 def six_key_to_array(six_key: jnp.array, dtype: any) -> jnp.array:
     """Convert six keys to six element array"""
     six_elements = jax.vmap(lambda x: jax.random.bits(x, dtype=dtype))(six_key)
@@ -23,21 +24,19 @@ def G(random_bits: jnp.array, unpack=True) -> jnp.array:
     """Let G : {0, 1}^λ → {0, 1}^2(2λ+1) be a pseudorandom generator.
     We take lambda = 32 and use uint32 to represent the bit string array.
 
-    input shape (n,) uint32 jnp array
-    output shape (n, 6) uint32 jnp array
+    input shape (1,) uint32 jnp array
+    output shape (1, 6) uint32 jnp array
     """
 
-    # assume random_bits is a byte array of shape (n, )
     # convert byte array into keys
-    keys = jax.vmap(jax.random.key)(random_bits)
+    key = jax.random.key(random_bits)
 
     # split keys into 6
-    # shape (n, 6) of keys
-    six_keys = jax.vmap(lambda x: jax.random.split(x, 6))(keys)
+    six_keys = jax.random.split(key, 6)
 
     # generate 6 new random bit arrays of shape lambda | lambda | 1 | lambda | lambda | 1
 
-    six_arrs = jax.vmap(lambda x: six_key_to_array(x, random_bits.dtype))(six_keys)
+    six_arrs = six_key_to_array(six_keys, random_bits.dtype)
     if unpack:
         return [six_arrs[i] for i in range(6)]
     else:
@@ -65,7 +64,7 @@ def get_nth_bit(num, n):
 
 def bit_decompose(x: jnp.uint64) -> List[int]:
     """convert an integer to a list of bits"""
-    return [get_nth_bit(x, i) for i in range(GROUP_BIT_NUM-1, -1, -1)]
+    return [get_nth_bit(x, i) for i in range(GROUP_BIT_NUM - 1, -1, -1)]
 
 
 def print_byte_in_binary(byte_value):
@@ -85,7 +84,7 @@ def DCF_gen(lamb: int, alpha: jnp.uint64, beta: jnp.uint64, key: int = 1212):
     random_key, new_random_key = jax.random.split(random_key, 2)
     s0_0 = jax.random.bits(random_key, dtype=lamb_type)
     s1_0 = jax.random.bits(new_random_key, dtype=lamb_type)
-    Valpha = jax.uint64(0)
+    Valpha = jnp.uint64(0)
     t0_0 = 0
     t1_0 = 1
     # paper is indexed from 1 to 64, but we start from 0
@@ -109,11 +108,14 @@ def DCF_gen(lamb: int, alpha: jnp.uint64, beta: jnp.uint64, key: int = 1212):
         s0_Lose = locals()['s0_' + Lose]
         s1_Lose = locals()['s1_' + Lose]
         v1_Lose = locals()['v1_' + Lose]
-        v0_Lose = locals()['t1_' + Lose]
+        v0_Lose = locals()['v0_' + Lose]
+
         s0_Keep = locals()['s0_' + Keep]
         s1_Keep = locals()['s1_' + Keep]
         t0_Keep = locals()['t0_' + Keep]
         t1_Keep = locals()['t1_' + Keep]
+        v1_Keep = locals()['v1_' + Keep]
+        v0_Keep = locals()['v0_' + Keep]
 
         sCW = jnp.bitwise_xor(s0_Lose, s1_Lose)
 
@@ -122,8 +124,6 @@ def DCF_gen(lamb: int, alpha: jnp.uint64, beta: jnp.uint64, key: int = 1212):
         if Lose == 'L':
             VCW = VCW + (-1) ** (t1_last) * beta
 
-        v1_Keep = locals()['v1_' + Keep]
-        v0_Keep = locals()['v0_' + Keep]
         Valpha = (
             Valpha - convert_G(v1_Keep) + convert_G(v0_Keep) + (-1) ** (t1_last) * VCW
         )
@@ -138,7 +138,7 @@ def DCF_gen(lamb: int, alpha: jnp.uint64, beta: jnp.uint64, key: int = 1212):
         s1_last = jnp.bitwise_xor(s1_Keep, t1_last * sCW)
         t0_last = jnp.bitwise_xor(t0_Keep, VCW)
         t1_last = jnp.bitwise_xor(t1_Keep, VCW)
-    CW = (-1) ** (t1_last)(convert_G(s1_last) - convert_G(s0_last) - Valpha)
+    CW = (-1) ** (t1_last) * (convert_G(s1_last) - convert_G(s0_last) - Valpha)
     CWs.append(CW)
     k0 = [s0_0] + CWs
     k1 = [s1_0] + CWs
@@ -158,7 +158,7 @@ def DCF_eval(b: int, kb: List, x: jnp.uint64):
     x_bits = bit_decompose(x)
     V = 0
     t = b
- 
+
     for i in range(GROUP_BIT_NUM):
         sCW, VCW, tCW_L, tCW_R = CWs[i]
         s_hat_L, v_hat_L, t_hat_L, s_hat_R, v_hat_R, t_hat_R = G(s)
@@ -171,10 +171,19 @@ def DCF_eval(b: int, kb: List, x: jnp.uint64):
             s = s_L
             t = t_L
         else:
+            V = V + (-1) ** b * (convert_G(v_hat_R) + t * VCW)
             s = s_R
             t = t_R
     V = V + (-1) ** b * (convert_G(s) + t * CWs[GROUP_BIT_NUM])
     return V
 
+
 if __name__ == '__main__':
-    print(convert_G("10101"))
+    # generate key for function < 0 return -1, else 0
+    k0, k1 = DCF_gen(64, 0, -1, 123)
+    print(k0, k1)
+
+    eval0 = DCF_eval(0, k0, 1000)
+    eval1 = DCF_eval(1, k1, 1000)
+    print(eval0, eval1, type(eval0), type(eval1))
+    assert eval0 + eval1 == 0, f"eval0 + eval1 != 0,{eval0+eval1}"
